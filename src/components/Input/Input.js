18268bb50032ae4e5fd4f45d37024a402814c136
Erik Pantzar <erik@epantzar.se>
2020-11-13 23:22:57 +0100

    Update touches

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -24,7 +24,7 @@ const Input = ({ label, type = 'text', placeholder, icon, inlineLabel, subLabel,
         className={[
           "Input-container", 
           icon && "Input-container--has-icon",
-          isFocused  && "Input-container--is-Focused" 
+          isFocused && "Input-container--is-Focused"
         ].join(' ')}
       >
         {icon && <span className="Input-Icon">{icon}</span>}
