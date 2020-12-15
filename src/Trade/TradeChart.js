dee86b0e10858a3a1de89cf98991672c1ece7e3d
Erik Pantzar <erik@epantzar.se>
2020-12-15 09:03:15 +0100

    TradeChart container grow

--- src/Trade/TradeChart.js
+++ src/Trade/TradeChart.js
@@ -1,20 +1,12 @@
 import React, { Fragment, useState, useContext } from 'react'
-import TradingViewWidget, { Themes } from 'react-tradingview-widget';
-import {
-  TabNavigator,
-  ButtonNavigator,
-  Typography,
-  Button,
-  Modal,
-} from '../components'
+import TradingViewWidget, { Themes } from 'react-tradingview-widget'
 
 const TradeChart = () => (
-    <TradingViewWidget 
-     symbol="BINANCE:BTCUSDT"
-     theme={Themes.DARK}
-     hide_side_toolbar={false}
-     />
-  )
-
+  <TradingViewWidget
+    symbol="BINANCE:BTCUSDT"
+    theme={Themes.DARK}
+    hide_side_toolbar={false}
+  />
+)
 
 export default TradeChart
