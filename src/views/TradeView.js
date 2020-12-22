669a1b19fe02dcf839298724814fa905ba448ba5
Erik Pantzar <erik@epantzar.se>
2020-12-22 08:55:23 +0100

    WIP

--- src/views/TradeView.js
+++ src/views/TradeView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import TradeContainer from '../Trade/TradeContainer'
 
 const TradeView = () => (
-  <main className="TradeView-Container">
+  <main className="TradeView-Container Trade">
     <TradeContainer />
   </main>
 )
