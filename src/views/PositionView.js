55a34b1f255fff0e51869bdc10ebe9bbc22bc445
Artur Silva <artursilva@boldint.com>
2021-01-12 18:27:55 +0000

    Position position view padding Top & Portfolio refresh button add icon

--- src/views/PositionView.js
+++ src/views/PositionView.js
@@ -4,7 +4,7 @@ import { data, cols } from '../Position/utils/mock-data'
 
 const Position = () => {
   return (
-    <div className="container">
+    <div className="container" style={{ paddingTop: '48px' }}>
       <AccordionContainer data={data} columns={cols} />
     </div>
   )
