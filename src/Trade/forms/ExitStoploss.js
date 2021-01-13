95b44f7101b9a140561c8ae888b8263615810b9c
Jason Quek <jason.quek@avalonsolutions.com>
2021-01-13 22:42:18 +0100

    add sliders

--- src/Trade/forms/ExitStoploss.js
+++ src/Trade/forms/ExitStoploss.js
@@ -3,6 +3,23 @@ import { InlineInput, Button, TabNavigator, Typography } from '../../components'
 import { TradeContext } from '../context/SimpleTradeContext'
 import roundNumbers from '../../helpers/roundNumbers'
 import { useSymbolContext } from '../context/SymbolContext'
+import Slider from 'rc-slider'
+import Grid from '@material-ui/core/Grid'
+import 'rc-slider/assets/index.css'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    width: 330,
+  },
+  slider: {
+    width: 200,
+    vertiicalAlign: 'middle'
+  },
+  input: {
+    width: 42,
+  },
+});
 
 const ExitStoploss = () => {
   const { isLoading, selectedSymbolDetail } = useSymbolContext()
@@ -14,6 +31,57 @@ const ExitStoploss = () => {
   const [quantity, setQuantity] = useState('')
   const [quantityPercentage, setQuantityPercentage] = useState('')
   const [isValid, setIsValid] = useState(false)
+
+  const classes = useStyles()
+  const marks = {
+    '-100': '',
+    '-75': '',
+    '-50': '',
+    '-25': '',
+    '0': ''
+  }
+
+  const handleSliderChange = (newValue) => {
+    newValue = 0 - newValue
+    setProfit(newValue)
+    priceAndProfitSync('profit', newValue)
+  }
+
+  const handleInputChange = (value) => {
+    value = 0 - value
+    setProfit(value === '' ? '' : Number(value));
+    priceAndProfitSync('profit', value)
+  }
+
+  const handleBlur = () => {
+    if (quantityPercentage < 0) {
+      setProfit(0)
+      priceAndProfitSync('profit', 0)
+    } else if (quantityPercentage > 100) {
+      setProfit(100)
+      priceAndProfitSync('profit', 100)
+    }
+  }
+
+  const handleQPSliderChange = (newValue) => {
+    setQuantityPercentage(newValue)
+    priceAndProfitSync('quantityPercentage', newValue)
+  }
+
+  const handleQPInputChange = (value) => {
+    setQuantityPercentage(value === '' ? '' : Number(value));
+    priceAndProfitSync('quantityPercentage', value)
+  }
+
+  const handleQPBlur = () => {
+    if (quantityPercentage < 0) {
+      setQuantityPercentage(0)
+      priceAndProfitSync('quantityPercentage', 0)
+    } else if (quantityPercentage > 100) {
+      setQuantityPercentage(100)
+      priceAndProfitSync('quantityPercentage', 100)
+    }
+  }
 
   useEffect(
     () => {
@@ -102,21 +170,37 @@ const ExitStoploss = () => {
             postLabel={ selectedSymbolDetail['quote_asset'] }
           />
 
-          <InlineInput
-            label="Profit"
-            type="number"
-            placeholder=""
-            onChange={(value) => {
-              if (value < 0) {
-                setProfit(value)
-              } else {
-                setProfit(-value)
-              }
-              priceAndProfitSync('profit', value)
-            }}
-            value={profit}
-            postLabel="%"
-          />
+          <div className={classes.root}>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item>
+                <Typography>
+                Profit
+                </Typography>
+              </Grid>
+              <Grid item xs className={classes.slider}>
+                <Slider
+                  reverse
+                  defaultValue={0}
+                  step={1}
+                  marks={marks}
+                  min={0}
+                  max={100}
+                  onChange={handleSliderChange}
+                  value={0-profit}
+                /> 
+              </Grid>
+              <Grid item>
+                <InlineInput
+                  className={classes.input}
+                  value={profit}
+                  margin="dense"
+                  onChange={handleInputChange}
+                  onBlur={handleBlur}
+                  postLabel={ '%' }
+                />
+              </Grid>
+            </Grid>
+          </div>
 
           <InlineInput
             label="Amount"
@@ -129,19 +213,32 @@ const ExitStoploss = () => {
             postLabel={isLoading ? "" : selectedSymbolDetail['base_asset']}
           />
 
-          <InlineInput
-            label=""
-            value={quantityPercentage}
-            onChange={(value) => {
-              setQuantityPercentage(value)
-              priceAndProfitSync('quantityPercentage', value)
-            }}
-            type="number"
-            min={-100}
-            max={100}
-            placeholder=""
-            postLabel="%"
-          />
+          <div className={classes.root}>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs className={classes.slider}>
+                <Slider
+                
+                  defaultValue={0}
+                  step={1}
+                  marks={marks}
+                  min={0}
+                  max={100}
+                  onChange={handleQPSliderChange}
+                  value={quantityPercentage}
+                /> 
+              </Grid>
+              <Grid item>
+                <InlineInput
+                  className={classes.input}
+                  value={quantityPercentage}
+                  margin="dense"
+                  onChange={handleQPInputChange}
+                  onBlur={handleQPBlur}
+                  postLabel={ '%' }
+                />
+              </Grid>
+            </Grid>
+          </div>
 
           <Button
             type="submit"
