13b5bae0966f289ce7d23da8900a2a15d630e51f
Artur Silva <artursilva@boldint.com>
2021-01-19 13:05:00 +0000

    Fulltrade input precision functions

--- src/components/Validation/Validation.js
+++ src/components/Validation/Validation.js
@@ -5,15 +5,24 @@ export default function validateFields(values) {
     values[el] = parseFloat(values[el])
   })
 
-  if (!values.price || values.price === 0) {
-    console.log(values.price)
+  if (!values.price) {
     errors.price = 'Price is required'
+  } else {
+    if (values.price < values.minPrice) {
+      errors.price = 'Price needs to meet min-price-total'
+    }
+
+    if (values.price > values.maxPrice) {
+      errors.price = 'Price needs to meet max-price-total'
+    }
   }
-  if (values.price > values.maxPrice) {
-    errors.price = 'Price needs to meet max-price-total'
+
+  if (values.quantity < values.minQty) {
+    errors.quantity = 'Amount needs to meet min-amount-total'
   }
-  if (values.price < values.minPrice) {
-    errors.price = 'Price needs to meet min-price-total'
+
+  if (values.quantity > values.maxQty) {
+    errors.quantity = 'Amount needs to meet min-amount-total'
   }
 
   if (!values.quantity || values.quantity === 0) {
