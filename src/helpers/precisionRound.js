2c6d6a7f71c0c1601ea44517f91654614f725b67
Artur Silva <artursilva@boldint.com>
2021-01-25 15:07:34 +0000

    full-trade form input precision updates remove onBlur precison updates

--- src/helpers/precisionRound.js
+++ src/helpers/precisionRound.js
@@ -2,7 +2,7 @@ const precisionRound = (input, precision = 2) => {
   if (precision > 5) {
     return ((Number(input) * 1000000) / 1000000).toFixed(precision)
   }
-  return Math.floor((Number(input) + Number.EPSILON) * 100) / 100
+  return ((Number(input) * 10) / 10).toFixed(precision)
 }
 
 export default precisionRound
