3225886de7bf0332a117bcc13c61e478bbe6ace1
gz89 <devjiang89@gmail.com>
2021-02-09 22:08:58 +0300

    [CP-324] Update order confirmation modal

--- src/Trade/components/TradeModal/TradeModal.js
+++ src/Trade/components/TradeModal/TradeModal.js
@@ -1,68 +1,114 @@
 import React, { Fragment, useContext } from 'react'
 import { Typography, Button } from '../../../components'
 import { TradeContext } from '../../context/SimpleTradeContext'
+import { useSymbolContext } from '../../context/SymbolContext'
 import roundNumbers from '../../../helpers/roundNumbers'
 import styles from './TradeModal.module.css'
 
 const TradeModal = ({ onClose, placeOrder }) => {
   const { state } = useContext(TradeContext)
+  const orderType = {
+    limit: 'Limit',
+    'stop-limit': 'Stop-Limit',
+    'stop-market': 'Stop-Market',
+  }
+
+  const { selectedSymbolDetail } = useSymbolContext()
 
   return (
     <article className={styles['TradeModal-Container']}>
+      <div className={styles['TradeModal-close']}>
+        <Button onClick={onClose} remove>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-x"
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </Button>
+      </div>
       <header className={styles['TradeModal-header']}>
-        <Typography as="h2">Order confirmation</Typography>
+        <Typography as="h4">{state.entry.symbol} Full Trade</Typography>
+        <Typography className={styles['TradeModal-header__description']}>
+          {state.entry.type} Buy
+        </Typography>
       </header>
 
       <main className={styles['TradeModal-Main']}>
-        <Typography>The trade</Typography>
-
-        <Typography>
-          Entry price:
-          {state.entry.price}
-        </Typography>
-        <Typography>
-          Entry amount:
-          {state.entry.quantity}
-        </Typography>
-
-        <Typography as="h3">Stoploss</Typography>
-
-        {state.stoploss.map((stoploss, index) => (
-          <Fragment key={index}>
+        <div className={styles['TradeModal-Main__entry']}>
+          <Typography>Price</Typography>
+          {state.entry.type === 'market' ? (
+            <Typography>Market Price</Typography>
+          ) : (
             <Typography>
-              Trigger price:
-              {stoploss.triggerPrice}
+              {state.entry.price} {selectedSymbolDetail['quote_asset']}
             </Typography>
-
+          )}
+        </div>
+        <div className={styles['TradeModal-Main__entry']}>
+          <Typography>Order Qty</Typography>
+          {state.entry.quantity && (
             <Typography>
-              Amount: {stoploss.quantity} (
-              {roundNumbers(stoploss.quantity / state.entry.quantity) * 100}
-              %)
+              {state.entry.quantity} {selectedSymbolDetail['base_asset']}
             </Typography>
-          </Fragment>
-        ))}
-
-        <Typography as="h3">Target(s)</Typography>
-
-        {state.targets.map((target, index) => (
-          <Fragment key={index}>
-            <Typography>Target price: {target.price}</Typography>
-
-            <Typography>
-              Amount: {target.quantity} (
-              {roundNumbers(target.quantity / state.entry.quantity) * 100}%)
-            </Typography>
-          </Fragment>
-        ))}
-
-        <Typography>Trade summary</Typography>
+          )}
+        </div>
+        <div className={styles['TradeModal-Main-Table']}>
+          <table>
+            <tbody>
+              {state.targets.map((target, index) => (
+                <tr key={index}>
+                  <td>
+                    Target {index + 1} ({orderType[target.type]})
+                  </td>
+                  <td>
+                    (
+                    {roundNumbers(target.quantity / state.entry.quantity) * 100}
+                    %) {target.quantity} {selectedSymbolDetail['base_asset']}
+                  </td>
+                  <td>
+                    {target.price} {selectedSymbolDetail['quote_asset']}
+                  </td>
+                </tr>
+              ))}
+              {state.stoploss.map((stoploss, index) => (
+                <tr key={index}>
+                  <td>Stop Loss ({orderType[stoploss.type]})</td>
+                  <td>
+                    (
+                    {roundNumbers(stoploss.quantity / state.entry.quantity) *
+                      100}
+                    %) {stoploss.quantity} {selectedSymbolDetail['base_asset']}
+                  </td>
+                  <td>
+                    {stoploss.type === 'stop-market'
+                      ? stoploss.triggerPrice
+                      : stoploss.price}{' '}
+                    {selectedSymbolDetail['quote_asset']}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </main>
 
       <footer className={styles['TradeModal-Footer']}>
-        <Button onClick={onClose} variant="cancel">Cancel</Button>
-        <Button variant="confirm" onClick={placeOrder}>
-          Confirm
-        </Button>
+        <button onClick={onClose} className="btn btn-danger">
+          Cancel
+        </button>
+        <button onClick={placeOrder} className="btn btn-success">
+          Confirm Order
+        </button>
       </footer>
     </article>
   )
