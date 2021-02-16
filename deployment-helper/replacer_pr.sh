510e8e70883eae7bce0cfacaa4bb2adf73ec25ed
Ábel Szabó <helloinfi@gmail.com>
2021-02-16 10:42:18 +0100

    Baseurl modification for PRs

--- deployment-helper/replacer_pr.sh
+++ deployment-helper/replacer_pr.sh
@@ -1,3 +1,4 @@
 sed s/"<IMAGE_VERSION>"/$1/g deployment_pr_template.yaml > deployment_1.yaml
 sed s/"<GCP_PROJECT>"/$2/g deployment_1.yaml > deployment_2.yaml
 sed s/"<IMAGE_NAME>"/$3/g deployment_2.yaml > deployment_pr.yaml
+sed -n 'H;${x;s/^\n//;s/"eslintConfig.*$/"homepage" : "https://dev.coinpanel.com/pr/$3",\n&/;p;}' package.json
