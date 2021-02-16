246ce3c44a4c80a5f8af1dc21f2b8c8736c696f1
Sharjeel Imtiaz <imtiazsharjeel@gmail.com>
2021-02-16 16:05:38 +0500

    Minor change

--- src/Portfolio/components/Chart/PieCharts.js
+++ src/Portfolio/components/Chart/PieCharts.js
@@ -9,7 +9,7 @@ const PieCharts = () => {
   const [data, setData] = useState([])
   const [extData, setExtData] = useState(null)
   const { chart } = useContext(PortfolioContext)
-  let colorArray = ['', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+  let colorArray = ['#E64D66', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
     '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
     '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
     '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
