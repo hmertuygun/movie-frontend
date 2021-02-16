a4870aad267dc35eb4b43ade56e602991a62fbfd
Ábel Szabó <helloinfi@gmail.com>
2021-02-16 13:22:21 +0100

    Baseurl modification for PRs 6

--- deployment-helper/replacer_pr.sh
+++ deployment-helper/replacer_pr.sh
@@ -1,5 +1,3 @@
 sed s/"<IMAGE_VERSION>"/$1/g deployment_pr_template.yaml > deployment_1.yaml
 sed s/"<GCP_PROJECT>"/$2/g deployment_1.yaml > deployment_2.yaml
 sed s/"<IMAGE_NAME>"/$3/g deployment_2.yaml > deployment_pr.yaml
-sed s/"true,"/"true,\n  \"homepage\" : \"https:\/\/dev.coinpanel.com\/pr\/$3\","/ package.json > package2.json
-mv package2.json package.json
