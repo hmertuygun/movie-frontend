e7cf99aef433bf475d0d915ccd0b455d282959d8
Sharjeel Imtiaz <imtiazsharjeel@gmail.com>
2021-03-23 20:57:26 +0500

    Fix cahrt interval break

--- src/api/binanceAPI.js
+++ src/api/binanceAPI.js
@@ -4,6 +4,7 @@ export default class binanceAPI {
     this.binanceHost = 'https://api.binance.com'
     this.debug = options?.debug || false
     this.ws = new socketClient()
+    this.lastDate = null
   }
 
   binanceServerTime() {
@@ -108,33 +109,77 @@ export default class binanceAPI {
     onResolveErrorCallback('not found')
   }
 
-  async getBars(symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
-    try {
-      let interval = this.ws.tvIntervals[resolution]
-      to *= 1000
-      from *= 1000
-      console.log(`Chart Date Range | ${new Date(from)} - ${new Date(to)}`)
-      //console.log(`Interval: ${resolution}`)
-      let data = await this.binanceKlines(symbolInfo.name, interval, null, to)
-      console.log(`First Data: ${new Date(data[0][0])} | Last Data: ${new Date(data[data.length - 1][0])}`)
+  getBars(symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
+    const interval = {
+      '1': '1m',
+      '3': '3m',
+      '5': '5m',
+      '15': '15m',
+      '30': '30m',
+      '60': '1h',
+      '120': '2h',
+      '240': '4h',
+      '360': '6h',
+      '480': '8h',
+      '720': '12h',
+      'D': '1d',
+      '1D': '1d',
+      '3D': '3d',
+      'W': '1w',
+      '1W': '1w',
+      'M': '1M',
+      '1M': '1M',
+    }[resolution]
 
-      if (!data || !data.length) onHistoryCallback([], { noData: true })
-      else {
-        data = data.map(item => ({
-          time: item[0],
-          open: parseFloat(item[1]),
-          high: parseFloat(item[2]),
-          low: parseFloat(item[3]),
-          close: parseFloat(item[4]),
-          volume: parseFloat(item[5])
-        }))
-        onHistoryCallback(data, { noData: false })
+    if (!interval) {
+      onErrorCallback('Invalid interval')
+    }
+
+    let totalKlines = []
+
+    const finishKlines = () => {
+      if (this.debug) {
+        console.log('📊:', totalKlines.length)
       }
-      console.log(`*****************************************************`)
+
+      if (totalKlines.length == 0) {
+        onHistoryCallback([], { noData: true })
+      } else {
+        onHistoryCallback(totalKlines.map(kline => {
+          return {
+            time: kline[0],
+            close: parseFloat(kline[4]),
+            open: parseFloat(kline[1]),
+            high: parseFloat(kline[2]),
+            low: parseFloat(kline[3]),
+            volume: parseFloat(kline[5])
+          }
+        }), {
+          noData: false
+        })
+      }
     }
-    catch (e) {
-      console.error(e)
+
+    const getKlines = (from, to) => {
+      this.binanceKlines(symbolInfo.name, interval, from, to, 500).then(klines => {
+        totalKlines = totalKlines.concat(klines)
+
+        if (klines.length == 500) {
+          from = klines[klines.length - 1][0] + 1
+          getKlines(from, to)
+        } else {
+          finishKlines()
+        }
+      }).catch(err => {
+        console.error(err)
+        onErrorCallback('Some problem')
+      })
     }
+
+    from *= 1000
+    to *= 1000
+
+    getKlines(from, to)
   }
 
   subscribeBars(symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback) {
