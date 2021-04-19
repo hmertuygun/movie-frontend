b66a6af6b5f8febdc769e04a3970abee40764dad
gz89 <devjiang89@gmail.com>
2021-04-19 16:11:05 +0300

    Update Symbol Dropdown

--- src/Trade/components/SymbolSelect/SymbolSelect.js
+++ src/Trade/components/SymbolSelect/SymbolSelect.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useMemo, useContext } from 'react'
 import { useSymbolContext } from '../../context/SymbolContext'
 import { UserContext } from '../../../contexts/UserContext'
 import styles from './SymbolSelect.module.css'
@@ -16,8 +16,9 @@ const SymbolSelect = () => {
   } = useSymbolContext()
 
   const { activeExchange } = useContext(UserContext)
+
   const customStyles = {
-    control: (styles, { }) => ({
+    control: (styles, {}) => ({
       ...styles,
       padding: '0px',
       width: '300px',
@@ -37,15 +38,23 @@ const SymbolSelect = () => {
     }),
   }
 
+  const selectedSymbols = useMemo(() => {
+    const { exchange } = activeExchange
+    const selected = symbols.filter((symbol) =>
+      symbol.value.toLowerCase().includes(exchange.toLowerCase())
+    )
+    return selected
+  }, [symbols, activeExchange])
+
   const handleSearch = ({ state }) => {
-    const filteredData = Object.values(symbols).filter((search) =>
+    const filteredData = Object.values(selectedSymbols).filter((search) =>
       search.label
         .split('-')[0]
         .toLowerCase()
         .includes(state.search.toLowerCase())
     )
     if (!filteredData.length) {
-      return Object.values(symbols).filter((search) =>
+      return Object.values(selectedSymbols).filter((search) =>
         search.label.toLowerCase().includes(state.search.toLowerCase())
       )
     }
@@ -68,10 +77,9 @@ const SymbolSelect = () => {
 
       <div className={styles['Select-Container']}>
         <Select
-          options={Object.values(symbols)}
+          options={Object.values(selectedSymbols)}
           placeholder="Select trading pair"
           values={[selectedSymbol]}
-          valueField="label"
           onChange={(value) => setSymbol(value[0])}
           disabled={isLoadingBalance || isLoading}
           searchFn={handleSearch}
