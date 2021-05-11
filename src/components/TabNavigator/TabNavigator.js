c518f78c15946d595209bde95067865c40091515
gz89 <devjiang89@gmail.com>
2021-05-11 02:42:44 -0700

    Update trade order info modal

--- src/components/TabNavigator/TabNavigator.js
+++ src/components/TabNavigator/TabNavigator.js
@@ -23,8 +23,11 @@ const TabInfo = () => {
             Stop Limit
           </a>
           <p>
-            Stop Limit Orders execute when a specified stop price is reached.
-            Specify a trigger price to activate the order.
+            A stop-limit order consists of Trigger Price and Limit Price. The
+            trigger price is simply the price that triggers a limit order, and
+            the limit price is the specific price of the limit order that was
+            triggered. This means that once your trigger price has been reached,
+            your limit order will be immediately placed on the order book.
           </p>
           <a
             href="https://support.coinpanel.com/hc/en-us/articles/360018782039-What-is-a-Stop-Market-order-"
@@ -34,8 +37,36 @@ const TabInfo = () => {
             Stop Market
           </a>
           <p>
-            Stop Market Orders execute when a specified price is reached.
-            Specify a trigger price to activate the order.
+            A stop-market order uses a Trigger Price as a trigger. When the
+            trigger price is reached, it triggers a market order.
+          </p>
+          <a
+            href="https://support.coinpanel.com/hc/en-us/articles/360020673540-What-is-a-Take-Profit-Limit-Order-"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Take Profit Limit
+          </a>
+          <p>
+            A take-profit-limit order consists of Trigger Price and Limit Price.
+            The trigger price is simply the price that triggers a limit order,
+            and the limit price is the specific price of the limit order that
+            was triggered. This means that once your trigger price has been
+            reached, your limit order will be immediately placed on the order
+            book.
+          </p>
+          <a
+            href="https://support.coinpanel.com/hc/en-us/articles/360020676680-What-is-a-Take-Profit-Market-order-"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Take Profit Market
+          </a>
+          <p>
+            A take-profit-market order uses a Trigger Price as a trigger. When
+            the trigger price is reached, it triggers a market order. Make sure
+            you learn about market orders before using take-profit-market
+            orders.
           </p>
         </div>
       )}
@@ -49,7 +80,7 @@ const TabNavigator = ({
   children,
   hadDropDown = true,
 }) => {
-  const { showMarketItems, tour2CurrentStep } = useContext(UserContext);
+  const { showMarketItems, tour2CurrentStep } = useContext(UserContext)
   const [contentIndex, setContentIndex] = useState(index)
   const [selectedDropDownOption, setSelectedDropdownOption] = useState(
     labelArray[2]
@@ -82,7 +113,10 @@ const TabNavigator = ({
 
   return (
     <>
-      <div className="TabNavigator-container" id={labelArray[0] === 'Limit' ? 'cp-tour4' : ''}>
+      <div
+        className="TabNavigator-container"
+        id={labelArray[0] === 'Limit' ? 'cp-tour4' : ''}
+      >
         {hadDropDown ? (
           <nav className="TabNavigator-nav DropDownNav">
             {labelArray.slice(0, 2).map((label, labelIndex) => (
@@ -115,7 +149,11 @@ const TabNavigator = ({
                 >
                   {selectedDropDownOption}
                 </button>
-                <div className={`chevron-wrapper ${showMarketItems && 'show'}`} id="cp-tour5-container" tabIndex="0">
+                <div
+                  className={`chevron-wrapper ${showMarketItems && 'show'}`}
+                  id="cp-tour5-container"
+                  tabIndex="0"
+                >
                   <ChevronDown
                     size={15}
                     color="#718096"
