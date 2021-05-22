31d33a5051dc969cd43cc3abdb94db94bcf3340e
Georgi <georgi.georgiev@atscale.com>
2021-05-23 02:14:55 +0300

    revert 328d815

--- src/Portfolio/components/BalanceTable.js
+++ src/Portfolio/components/BalanceTable.js
@@ -19,7 +19,6 @@ const BalanceTable = () => {
   const ITEMS_PER_PAGE = 8
 
   const getTableData = useMemo(() => {
-
     if (tableData) {
       let data = tableData
 
@@ -46,17 +45,41 @@ const BalanceTable = () => {
     }
   }, [balance, setTableData, getTableData])
 
-  const fetchLatestPrice = () => {
-    const tempBalance = balance
-    tempBalance.forEach((item, index, arr) => {
-      const fData = lastMessage.find((item1) => item1.symbol === `${item.SYMBOL}BTC`)
-      const fData1 = lastMessage.find((item1) => item1.symbol === `${item.SYMBOL}USD`)
-      console.log(`${item.SYMBOL}BTC`, fData)
-      console.log(`${item.SYMBOL}USD`, fData1)
-      if (fData) arr[index].BTC = (fData.lastPrice * item.TOTAL).toFixed(8)
-      if (fData1) arr[index].USD = (fData1.lastPrice * item.TOTAL).toFixed(8)
-    })
-    setTableData(tempBalance)
+  const fetchLatestPrice = async () => {
+    if (!activeExchange?.exchange) return
+    try {
+      const { exchange } = activeExchange
+      const tempBalance = balance
+      // const lastMessage = await getSymbolPriceTicker()
+      const proxy = window.location.hostname === "localhost" ? 'http://localhost:8080/' : 'https://nodejs-cors.herokuapp.com/'
+      const exchangeInit = exchange === "binance" ? new ccxt.binance() : new ccxt.ftx({ proxy: proxy })
+      const lastMessage = await exchangeInit.fetchTickers()
+      console.log(lastMessage)
+      const arrayData = Object.values(lastMessage)
+      tempBalance.forEach((item, index, arr) => {
+        const fData = arrayData.find((item1) => item1.symbol === `${item.SYMBOL}/BTC`)
+        const fData1 = arrayData.find((item1) => item1.symbol === `${item.SYMBOL}/USDT`)
+        // console.log(`${item.SYMBOL}/BTC`, fData)
+        // console.log(`${item.SYMBOL}/USDT`, fData1)
+        if (fData) arr[index].BTC = (fData.price * item.TOTAL).toFixed(8)
+        if (fData1) arr[index].USD = (fData1.price * item.TOTAL).toFixed(8)
+      })
+      setTableData(tempBalance)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  // useEffect(() => {
+  //   if (!balance?.length || !lastMessage?.length) return
+  //   fetchLatestPrice()
+  // }, [lastMessage])
+
+  const latestPricePolling = () => {
+    clearInterval(pricePolling)
+    pricePolling = null
+    pricePolling = setInterval(fetchLatestPrice, 3000)
   }
 
   useEffect(() => {
