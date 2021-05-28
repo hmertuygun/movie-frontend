38227480101a2a34439931036424a404b6183ce4
gz89 <devjiang89@gmail.com>
2021-05-28 08:06:49 -0700

    Dark mode for Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { ThemeContext } from '../../contexts/ThemeContext'; 
 import './Logo.css'
 
-const Logo = () => (
-  <Link to="/" className="logo">
-    <img
-      alt="Part one logo"
-      src="/img/svg/icons/Coinpanel_Color.svg"
-      className="colorlogo"
-    />
-    CoinPanel
-  </Link>
-)
+const Logo = () => {
+  const { theme } = useContext(ThemeContext);
+  const style = {
+    color: theme === "DARK" ? "white": "var(--blue)"
+  }
+
+  return (
+    <Link to="/" className="logo" style={style}>
+      <img
+        alt="Part one logo"
+        src="/img/svg/icons/Coinpanel_Color.svg"
+        className="colorlogo"
+      />
+      <img
+        alt="Part one logo"
+        src="/img/svg/icons/Coinpanel_Color.svg"
+        className="whitelogo"
+      />
+      CoinPanel
+    </Link>
+  )
+}
 
 export default Logo
