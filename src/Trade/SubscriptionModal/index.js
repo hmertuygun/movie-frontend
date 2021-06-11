64295e605e67a9e7a63ed8dd362e0463840b164e
sharjeel619 <imtiazsharjeel@gmail.com>
2021-06-11 20:02:25 +0500

    Sub modal changes (#546)

--- src/Trade/SubscriptionModal/index.js
+++ src/Trade/SubscriptionModal/index.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 import { UserContext } from '../../contexts/UserContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const SubscriptionModal = () => {
-  const { hasSub, isLoggedIn } = useContext(UserContext)
+  const { hasSub, isLoggedIn, showSubModalIfLessThan7Days, trialDaysLeft, setShowSubModal } = useContext(UserContext)
 
   const modalVisibility = () => {
     if (isLoggedIn) {
-      if (hasSub) return 'none'
-      else return 'block'
+      if (!hasSub || showSubModalIfLessThan7Days) return 'block'
+      else return 'none'
     }
     else {
       return 'none'
@@ -25,16 +25,16 @@ const SubscriptionModal = () => {
       <div className="modal-dialog modal-lg">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title h6">Renew Subscription</h5>
+            <h5 className="modal-title h6">{showSubModalIfLessThan7Days ? 'Trial ending reminder' : 'Renew Subscription'}</h5>
           </div>
           <div className="modal-body">
             <div className="d-flex align-items-center flex-column">
               <FontAwesomeIcon icon="exclamation-triangle" size="6x" color="#f25767" className="mb-3" />
               <p>
-                Uh oh! Looks like your CoinPanel subscription has expired. <br />
+                {showSubModalIfLessThan7Days ? `You have less than ${Math.ceil(trialDaysLeft)} days left. ` : 'Uh oh! Looks like your CoinPanel subscription has expired. '}
                 Click on the button below to renew your subscription.
               </p>
-              <button type="button" className="btn btn-primary btn-sm">
+              <button type="button" className="btn btn-primary btn-sm" onClick={() => setShowSubModal(false)}>
                 <Link to="/settings#subscription" className="text-light">Buy Subscription</Link>
                 <FontAwesomeIcon icon="external-link-alt" color="#ffffff" className="ml-1" />
               </button>
