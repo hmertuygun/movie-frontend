15c8a25d99f477895041cde2aa70541f6a79e9d3
Jeyanth <54062741+jeyk333@users.noreply.github.com>
2022-03-15 19:51:33 +0530

    Updated registeration page design (#1069)

---
+++ src/constants/RegisterContent.js
@@ -0,0 +1,9 @@
+export const CONTENT = [
+  'Easy stop-loss / take-profit orders',
+  'Place entry, stop loss, take profit at the same time',
+  'Automatic profit/loss tracking',
+  'Full suite of institutional-grade analytics tools',
+  'Follow analysts’ charts live',
+  'Comprehensive portfolio management across integrated exchanges',
+  'Email/Telegram notifications',
+]
