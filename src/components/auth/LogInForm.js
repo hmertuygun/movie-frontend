adb975863d9439cd56a30d7e447a2312fd8a4e98
Mert Uygun <uygun.hmert@gmail.com>
2022-07-30 16:40:52 +0300

    Implement Log In form

--- src/components/auth/LogInForm.js
+++ src/components/auth/LogInForm.js
@@ -1,17 +1,80 @@
-import { Button, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
-import React from "react";
+import React, { useMemo, useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { validateForm, validateInput } from "../../utils/validateForms";
+import prepareSchema from "../../utils/prepareSchema";
+import { useDispatch, useSelector } from "react-redux";
 
-const LogInForm = () => {
+import { loginUser } from "../../store/actions";
+import { Navigate } from "react-router-dom";
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({ password: "", email: "" });
+  const [formErrors, setFormErrors] = useState();
+  const [hasError, setHasError] = useState();
+  const dispatch = useDispatch();
+  const { users, token } = useSelector((state) => state.users);
+
+  const formSchema = useMemo(() => prepareSchema("LOGIN"), []);
+
+  const handleChange = async (e) => {
+    setFormData((values) => {
+      return { ...values, [e.target.name]: e.target.value };
+    });
+
+    const inputError = await validateInput(e.target, formSchema);
+
+    setFormErrors((errors) => {
+      return { ...errors, ...inputError };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const isValid = await validateForm(formErrors);
+    const isUserExists = users.find(
+      (user) =>
+        user.email === formData.email && user.password === formData.password,
+    );
+
+    if (isValid && isUserExists) {
+      dispatch(loginUser(formData.email));
+    }
+
+    return setHasError(true);
+  };
+
+  if (token) {
+    return <Navigate to="/" replace={true} />;
+  }
+
   return (
     <div>
       <Stack spacing={4}>
         <FormControl id="email">
           <FormLabel>Email address</FormLabel>
-          <Input type="email" />
+          <Input
+            name="email"
+            onChange={handleChange}
+            value={formData.email}
+            type="email"
+          />
         </FormControl>
         <FormControl id="password">
           <FormLabel>Password</FormLabel>
-          <Input type="password" />
+          <Input
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+            type="password"
+          />
         </FormControl>
         <Stack spacing={10}>
           <Button
@@ -20,13 +83,18 @@ const LogInForm = () => {
             _hover={{
               bg: "blue.500",
             }}
+            onClick={handleSubmit}
           >
             Sign in
           </Button>
         </Stack>
+
+        {hasError && (
+          <Box color={"red.300"}>Your email or password is incorrect</Box>
+        )}
       </Stack>
     </div>
   );
 };
 
-export default LogInForm;
+export default RegisterForm;
